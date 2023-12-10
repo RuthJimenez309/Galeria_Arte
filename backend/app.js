2e8e0b15dd5f676961f5fe468a470fffccb1186a
Liss3razo <erazolisbeth166@gmail.com>
2023-12-09 23:39:58 -0600

    act3

--- backend/app.js
+++ backend/app.js
@@ -7,21 +7,31 @@ function filtrarPorCategoria(images, categoria) {
 
 document.addEventListener('DOMContentLoaded', function () {
     const images = [
-        { path: '/backend/img/3.jpg', category: 'pintura' },
-        { path: '/backend/img/4.jpg', category: 'fotografia' },
-        { path: '/backend/img/5.jpg', category: 'pintura' },
-       
+        { path: '/backend/img/3.jpg', category: 'pintura', periodo: 'Renacimiento'},
+        { path: '/backend/img/4.jpg', category: 'fotografia', periodo: 'Contemporaneo' },
+        { path: '/backend/img/5.jpg', category: 'pintura', periodo: 'Barroco' },
+        { path: '/backend/img/image10.jpg', category: 'pintura', periodo: 'Renacimiento'},
+        { path: '/backend/img/image20.jpg', category: 'fotografia', periodo: 'Contemporaneo' },
+        { path: '/backend/img/image30.jpeg', category: 'pintura', periodo: 'Barroco' },
+        { path: '/backend/img/image11.jpg', category: 'pintura', periodo: 'Renacimiento'},
+        { path: '/backend/img/image21.jpg', category: 'fotografia', periodo: 'Contemporaneo' },
+        { path: '/backend/img/image31.jpeg', category: 'pintura', periodo: 'Barroco' },
+        { path: '/backend/img/image12.jpg', category: 'pintura', periodo: 'Renacimiento'},
+        { path: '/backend/img/image22.jpg', category: 'fotografia', periodo: 'Contemporaneo' },
+        { path: '/backend/img/image32.jpeg', category: 'pintura', periodo: 'Barroco' },
     ];
 
     const galleryElement = document.getElementById('galeriaFotos');
+    const statsElement = document.getElementById('stats');
 
     function mostrarTodasLasImagenes() {
         galleryElement.innerHTML = '';
 
-        images.forEach(function (imagen) {
+        images.map(function (imagen) {
             // Crear elemento de la tarjeta
             const cardElement = document.createElement('div');
             cardElement.classList.add('card', 'contenedorI');
+            cardElement.classList.add(imagen.category);
 
             // Crear elemento de la imagen dentro de la tarjeta
             const imgElement = document.createElement('img');
@@ -38,11 +48,12 @@ document.addEventListener('DOMContentLoaded', function () {
             cardTextElement.classList.add('card-text');
             cardTextElement.textContent = 'Some quick example text to build on the card title and make up the bulk of the card\'s content.';
             
-            
+
             // Anidar los elementos
             cardBodyElement.appendChild(cardTextElement);
             cardElement.appendChild(imgElement);
             cardElement.appendChild(cardBodyElement);
+            asignarMarcoEspecifico(cardElement, imagen);
             
             // Agregar la tarjeta al contenedor de la galería
             galleryElement.appendChild(cardElement);
@@ -51,6 +62,57 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Mostrar todas las imágenes al cargar la página
     mostrarTodasLasImagenes();
+    actualizarEstadisticas();
+
+    function asignarMarcoEspecifico(cardElement, imagen) {
+        // Utiliza map para asignar un color de marco específico según la categoría de la obra
+        const coloresMarcos = new Map([
+            ['pintura', 'red'],
+            ['fotografia', 'blue'],
+            // Agrega más categorías según sea necesario
+        ]);
+    
+        const categoria = imagen.category.toLowerCase();
+        const colorMarco = coloresMarcos.get(categoria) || 'black';
+    
+        // Agrega el estilo del marco a la tarjeta
+        cardElement.style.borderColor = colorMarco;
+    
+        // Agrega la información del marco a la tarjeta
+        const marcoElement = document.createElement('p');
+        marcoElement.textContent = `Color del Marco: ${colorMarco}`;
+        cardElement.appendChild(marcoElement);
+    }
+
+    function actualizarEstadisticas() {
+        const estadisticas = obtenerEstadisticasPorPeriodo();
+
+        // Muestra las estadísticas en el elemento correspondiente
+        mostrarEstadisticas(estadisticas);
+    }
+
+    function obtenerEstadisticasPorPeriodo() {
+        const estadisticas = images.reduce(function (accumulator, imagen) {
+            const periodo = imagen.periodo || 'Desconocido';
+            accumulator[periodo] = (accumulator[periodo] || 0) + 1;
+            return accumulator;
+        }, {});
+
+        return estadisticas;
+    }
+
+    function mostrarEstadisticas(estadisticas) {
+        statsElement.innerHTML = '<h2>Estadísticas por Período</h2>';
+        const ulElement = document.createElement('ul');
+
+        for (const periodo in estadisticas) {
+            const liElement = document.createElement('li');
+            liElement.textContent = `${periodo}: ${estadisticas[periodo]}`;
+            ulElement.appendChild(liElement);
+        }
+
+        statsElement.appendChild(ulElement);
+    }
 
     // Función para filtrar y mostrar imágenes por categoría
     function mostrarImagenesPorCategoria(categoria) {
@@ -59,6 +121,8 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         galleryElement.innerHTML = ''; // Limpiar galería
+
+
 
         imagenesFiltradas.forEach(function (imagen) {
             // Crear elemento de la tarjeta
